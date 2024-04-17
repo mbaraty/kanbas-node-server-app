fac5b792b81f51d5e2ebeaff1a6f8f6ba31f41d1
mbaraty123 <mbaraty123@gmail.com>
2024-04-16 22:53:56 -0400

    Integrated heroku+mongo atlas

--- App.js
+++ App.js
@@ -9,8 +9,8 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import session from "express-session"
 
-
-mongoose.connect("mongodb://localhost:27017/kanbas")
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
